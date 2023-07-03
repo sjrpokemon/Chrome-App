e6f2fb1a4c8af3a883b6a7762142678d75eeddf8
Joon Rhee <coldplay746@gmail.com>
2023-07-02 18:04:04 -0600

    Nomadcoder Chrome App (수업 수강 완료)
    코딩 더 배우면 웹사이트 더 가꾸기

---
+++ todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "toDos";
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    console.log(li.id);
+    li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item){
+    console.log("this is the turn of", item);
+}
+
+const savedtoDos = localStorage.getItem(TODOS_KEY);
+
+if(savedtoDos !== null){
+    const parsedToDos = JSON.parse(savedtoDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
+
+
---
+++ weather.js
@@ -0,0 +1,25 @@
+const API_KEY = "5eb555a76302d27c18d0cce07b92843e";
+
+
+
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
